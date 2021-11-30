82a7f16bf18ca2812f2067baf96aa0c39ec3da9a
Alejandro Cabrera <d14lga@MacBook-Air-de-Alejandro.local>
2021-11-30 16:43:53 -0600

    creamos una clase galleta y como entrar a mysql

--- clases.py
+++ clases.py
@@ -3,3 +3,11 @@
 # 2.- git add archivo.txt
 # 3.- git commit -m "aqui escribes un mensaje"
 # 4.- git push
+
+#Para correr mysql:
+# 1.- mysql -uroot
+class galleta:
+    def __init__(self, sabor:str):
+        self.sabor = sabor
+        self.tamaño = "sm"
+    
