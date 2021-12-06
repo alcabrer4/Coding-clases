55a76b5a0bd84167940a0be7bd2c0bcca413dfc6
Alejandro Cabrera <d14lga@MacBook-Air-de-Alejandro.local>
2021-12-06 17:24:46 -0600

    termine la base de datos

--- blockbuster.sql
+++ blockbuster.sql
@@ -4,91 +4,124 @@ CREATE DATABASE BLOCKBUSTER;
 
 USE BLOCKBUSTER;
 
-CREATE TABLE Tiendas(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    direccion VARCHAR(256) UNIQUE,
-    telefono BIGINT UNIQUE,
-    email VARCHAR(200) UNIQUE,
-    PRIMARY KEY (id)
+CREATE TABLE Videos_Stores(
+    store_id BIGINT NOT NULL AUTO_INCREMENT,
+    store_adress VARCHAR(512) UNIQUE,
+    store_phone BIGINT UNIQUE,
+    store_email VARCHAR(512) UNIQUE,
+    PRIMARY KEY (store_id)
 );
 
-CREATE TABLE Formatos(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    formato VARCHAR(100) UNIQUE,
-    PRIMARY KEY (id)
+CREATE TABLE Format_Types(
+    format_type_code BIGINT NOT NULL AUTO_INCREMENT,
+    format_type_description VARCHAR(512) UNIQUE,
+    PRIMARY KEY (format_type_code)
 );
 
-CREATE TABLE Generos(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    genero VARCHAR(100) UNIQUE,
-    PRIMARY KEY (id)
+CREATE TABLE Genres_Codes(
+    genre_code BIGINT NOT NULL AUTO_INCREMENT,
+    genre_description VARCHAR(512) UNIQUE,
+    PRIMARY KEY (genre_code)
 );
 
-CREATE TABLE Peliculas(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    formato_id BIGINT,
-    genero_id BIGINT,
-    tienda_id BIGINT,
-    año BIGINT,
-    titulo VARCHAR(100) UNIQUE,
-    en_stock BIGINT,
-    precio_renta FLOAT,
-    precio_compra FLOAT,
-    vendidas BIGINT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (formato_id) REFERENCES Formatos(id),
-    FOREIGN KEY (genero_id) REFERENCES Generos(id),
-    FOREIGN KEY (tienda_id) REFERENCES Tiendas(id)
+CREATE TABLE Movies(
+    movie_id BIGINT NOT NULL AUTO_INCREMENT,
+    format_type_code BIGINT,
+    genre_code BIGINT,
+    store_id BIGINT,
+    release_year BIGINT,
+    movie_title VARCHAR(512) UNIQUE,
+    number_in_stock BIGINT,
+    rental_daily_rate FLOAT,
+    sale_price FLOAT,
+    PRIMARY KEY (movie_id),
+    FOREIGN KEY (format_type_code) REFERENCES Format_Types(format_type_code),
+    FOREIGN KEY (genre_code) REFERENCES Genres_Codes(genre_code),
+    FOREIGN KEY (store_id) REFERENCES Videos_Stores(store_id)
 );
 
-CREATE TABLE Actores(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    apellido VARCHAR(100),
-    sexo VARCHAR(100),
-    PRIMARY KEY (id)
+CREATE TABLE Actors(
+    actor_id BIGINT NOT NULL AUTO_INCREMENT,
+    actor_first_name VARCHAR(512),
+    actor_last_name VARCHAR(512),
+    actor_gender VARCHAR(512),
+    PRIMARY KEY (actor_id)
 );
 
-CREATE TABLE Elenco(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    pelicula_id BIGINT UNIQUE,
+CREATE TABLE Movie_Cast(
+    movie_id BIGINT,
     actor_id BIGINT,
-    reparto_id BIGINT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (pelicula_id) REFERENCES Peliculas(id),
-    FOREIGN KEY (actor_id) REFERENCES Actores(id),
-    FOREIGN KEY (reparto_id) REFERENCES Actores(id)
+    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
+    FOREIGN KEY (actor_id) REFERENCES Actors(actor_id),
+    PRIMARY KEY (movie_id, actor_id)
 );
 
-CREATE TABLE Status_Renta(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    estado VARCHAR(100) UNIQUE,
-    PRIMARY KEY (id)
+CREATE TABLE Rental_Status_Codes(
+    rental_status_code BIGINT NOT NULL AUTO_INCREMENT,
+    rental_status_description VARCHAR(512) UNIQUE,
+    PRIMARY KEY (rental_status_code)
 );
 
-CREATE TABLE Clientes(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    apellido VARCHAR(100),
-    telefono BIGINT UNIQUE,
-    email VARCHAR(200) UNIQUE,
-    direccion VARCHAR(100),
-    PRIMARY KEY (id)
+CREATE TABLE Costumers(
+    costumer_id BIGINT NOT NULL AUTO_INCREMENT,
+    costumer_first_name VARCHAR(512),
+    costumer_last_name VARCHAR(512),
+    costumer_phone BIGINT UNIQUE,
+    costumer_email VARCHAR(512) UNIQUE,
+    costumer_address VARCHAR(512),
+    PRIMARY KEY (costumer_id)
 );
 
-CREATE TABLE Rentas_Clientes(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    cliente_id BIGINT,
-    pelicula_id BIGINT,
-    status_renta_id BIGINT,
-    dia_renta DATE,
-    dia_entrega DATE,
-    costo_renta FLOAT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (cliente_id) REFERENCES Clientes(id),
-    FOREIGN KEY (pelicula_id) REFERENCES Peliculas(id),
-    FOREIGN KEY (status_renta_id) REFERENCES Status_Renta(id)
+CREATE TABLE Costumer_Rentals(
+    item_rental_id BIGINT NOT NULL AUTO_INCREMENT,
+    costumer_id BIGINT,
+    movie_id BIGINT,
+    rental_status_code BIGINT,
+    rental_date_out DATE,
+    rental_date_returned DATE,
+    rental_amount_due FLOAT,
+    PRIMARY KEY (item_rental_id),
+    FOREIGN KEY (costumer_id) REFERENCES Costumers(costumer_id),
+    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id),
+    FOREIGN KEY (rental_status_code) REFERENCES Rental_Status_Codes(rental_status_code)
 );
 
-INSERT INTO Tiendas(direccion, telefono, email) VALUES ('casa', 0954, 'hola@mail.com'), 
-('house', 1234, 'adios@mail.com');
+CREATE TABLE Payment_Methods(
+    payment_method_code BIGINT NOT NULL UNIQUE,
+    payment_method_description VARCHAR(512) UNIQUE,
+    PRIMARY KEY (payment_method_code)
+);
+
+CREATE TABLE Accounts(
+    account_id BIGINT NOT NULL AUTO_INCREMENT,
+    costumer_id BIGINT,
+    payment_method_code BIGINT,
+    account_name VARCHAR(512) UNIQUE,
+    PRIMARY KEY (account_id),
+    FOREIGN KEY (costumer_id) REFERENCES Costumers(costumer_id),
+    FOREIGN KEY (payment_method_code) Payment_Methods(payment_method_code)
+);
+
+CREATE TABLE Transaction_Types(
+    transaction_type_code BIGINT NOT NULL AUTO_INCREMENT,
+    transaction_type_description VARCHAR(512) UNIQUE,
+    PRIMARY KEY (transaction_type_code)
+);
+
+CREATE TABLE Financial_Transactions(
+    transaction_id BIGINT NOT NULL AUTO_INCREMENT,
+    account_id BIGINT,
+    item_rental_id BIGINT,
+    transaction_type_code BIGINT,
+    transaction_date DATE,
+    transaction_ammount FLOAT,
+    PRIMARY KEY (transaction_id),
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id),
+    FOREIGN KEY (item_rental_id) REFERENCES Costumer_Rentals(item_rental_id),
+    FOREIGN KEY (transaction_type_code) REFERENCES Transaction_Types(transaction_type_code)
+);
+
+/* INSERT INTO Tiendas(direccion, telefono, email) VALUES ('casa', 0954, 'hola@mail.com'), 
+('house', 1234, 'adios@mail.com'); */
+
+
